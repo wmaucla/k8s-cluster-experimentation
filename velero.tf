2ca4bd21d67613de94769afbd2b3cc79c1a6ad5a
wmaucla <zhengweima@gmail.com>
2024-02-29 10:39:03 -0500

    working velero

---
+++ velero.tf
@@ -0,0 +1,26 @@
+resource "kubernetes_namespace" "velero" {
+  metadata {
+    labels = {
+      name = "velero"
+    }
+    name = "velero"
+  }
+}
+
+
+
+resource "helm_release" "velero" {
+  name      = "velero"
+  namespace = "velero"
+
+  repository = "https://vmware-tanzu.github.io/helm-charts"
+  chart      = "velero"
+  version    = "5.4.1"
+
+  timeout = "30"
+  values = [file("values.yaml")]
+
+  depends_on = [
+    kubernetes_namespace.velero
+  ]
+}
